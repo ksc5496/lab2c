01e6b7a4e3b37ae47c65f236f52f5fc83798a382
ksc5496 <ksc5496@psu.edu>
2020-09-10 14:09:36 +0000

    Added periods.

--- main.c
+++ main.c
@@ -17,4 +17,36 @@ int main(void) {
 
   input = readline("Enter your CMPSC 131 grade: ");
   Grade = atof(input);
+
+  if(Grade >= 93.0){
+    letterGrade = "A";
+  }
+  else if(Grade >= 90.0){
+    letterGrade = "A-";
+  }
+  else if(Grade >= 87.0){
+    letterGrade = "B+";
+  }
+  else if(Grade >= 83.0){
+    letterGrade = "B";
+  }
+  else if(Grade >= 80.0){
+    letterGrade = "B-";
+  }
+  else if(Grade >= 77.0){
+    letterGrade = "C+";
+  }
+  else if(Grade >= 70.0){
+    letterGrade = "C";
+  }
+  else if(Grade >= 60.0){
+    letterGrade = "D";
+  }
+  else{
+    letterGrade = "F";
+  }
+
+  printf("Your letter grade for CMPSC 131 is %s.\n", letterGrade);
+  return 0;
+  
 }
