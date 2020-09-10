56b6041efb2324a391784dfdec898517eb95ccb0
ksc5496 <ksc5496@psu.edu>
2020-09-10 13:49:20 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+// Author: Krish Choudhary ksc5496@psu.edu
+// Collaborator: Augustus Perseghin agp5191@psu.edu
+// Collaborator: Reuben Lee wzl128@psu.edu
+// Collabortor: Kieran Murdocca kkm5754@psu.edu
+// Section: 04
+// Breakout: 18
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int main(void) {
+  // variable definitions
+  char* letterGrade;
+  char* input;
+  float Grade;
+
+  input = readline("Enter your CMPSC 131 grade: ");
+  Grade = atof(input);
+}
